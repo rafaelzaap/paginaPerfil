4052c41431a680f84a9fb9aaa8642aac59f00d40
Rafael Silva <70986895+rafaelzaap@users.noreply.github.com>
2024-12-04 08:29:29 -0300

    comit inicial

---
+++ script.js
@@ -0,0 +1,6 @@
+const botaoMudarCor = document.getElementById("btn");
+const textNome = document.getElementById("nome");
+
+botaoMudarCor.addEventListener("click", function(){
+    textNome.textContent = "Rafael de Souza Silva";
+});
